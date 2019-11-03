f473e1c27580a84913946542ea5c069abc661262
hkdnet <satoko.itse@gmail.com>
2019-11-03 19:58:26 +0900

    campaign = 4

--- isucari/webapp/ruby/lib/isucari/web.rb
+++ isucari/webapp/ruby/lib/isucari/web.rb
@@ -269,7 +269,7 @@ module Isucari
 
       response = {
         # キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-        'campaign' => 0,
+        'campaign' => 4,
         # 実装言語を返す
         'language' => 'ruby',
       }
